b37fd3117ed53a2195420ef46542cf7479109ad9
Ayush Garg <humbleayush30@gmail.com>
2024-06-09 00:32:18 +0530

    Add files via upload

---
+++ dalle_clone/client/src/assets/index.js
@@ -0,0 +1,9 @@
+import download from './download.png';
+import logo from './srm-institute-of-science-and-technology-vector-logo-seeklogo/srm-institute-of-science-and-technology-seeklogo.svg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+};
